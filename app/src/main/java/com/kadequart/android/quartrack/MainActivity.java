e88862e5ea4797bf83adf207ed545fdb46a8902d
Jethro Ordaneza <jet.oneza@gmail.com>
2017-05-19 01:24:44 +0800

    Initial commit for MainActivity and TransactionFormActivity

---
+++ app/src/main/java/com/kadequart/android/quartrack/MainActivity.java
@@ -0,0 +1,28 @@
+package com.kadequart.android.quartrack;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button addButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        addButton = (Button) findViewById(R.id.add_button);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, TransactionFormActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/kadequart/android/quartrack/TransactionFormActivity.java
@@ -0,0 +1,48 @@
+package com.kadequart.android.quartrack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+
+public class TransactionFormActivity extends AppCompatActivity {
+
+    Spinner mTypeSpinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transaction_form);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Add New Transaction");
+
+        mTypeSpinner = (Spinner) findViewById(R.id.type_spinner);
+
+        ArrayList<String> types = new ArrayList<>();
+
+        types.add("Salary");
+        types.add("Payable");
+        types.add("Spend");
+        types.add("Donation");
+        types.add("Offering");
+
+        ArrayAdapter spinnerAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, types);
+        mTypeSpinner.setAdapter(spinnerAdapter);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
