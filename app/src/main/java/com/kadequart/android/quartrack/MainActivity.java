f6b90174af6f2d49879fd21876d0f255c8909083
Jethro Ordaneza <jet.oneza@gmail.com>
2017-05-19 01:53:40 +0800

    Convert ugly button to a fabulous floating button

--- app/src/main/java/com/kadequart/android/quartrack/MainActivity.java
+++ app/src/main/java/com/kadequart/android/quartrack/MainActivity.java
@@ -1,6 +1,7 @@
 package com.kadequart.android.quartrack;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,14 +9,14 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button addButton;
+    private FloatingActionButton addButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        addButton = (Button) findViewById(R.id.add_button);
+        addButton = (FloatingActionButton) findViewById(R.id.add_button);
 
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
